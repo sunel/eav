92f60b5ebba0c1d5ce8ebee5b60ac42b3bb89d4e
Sunel Tr <sunelbe@gmail.com>
2018-09-02 22:20:11 +0530

    added attributes to mass assigment

--- src/Attribute.php
+++ src/Attribute.php
@@ -30,7 +30,8 @@ class Attribute extends Model
         'attribute_code', 'backend_class', 'backend_type',
         'backend_table', 'frontend_class', 'frontend_type',
         'frontend_label', 'source_class',  'default_value',
-        'is_required', 'required_validate_class', 'entity_id'
+        'is_filterable', 'is_searchable',  'is_required', 
+        'required_validate_class', 'entity_id'
     ];
 
     /**
